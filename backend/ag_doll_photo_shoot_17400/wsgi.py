1bda785f992b702e77a571a0ad221e97b277d4a1
Crowdbotics <team@crowdbotics.com>
2020-05-26 03:42:07 +0000

    Update models

--- backend/ag_doll_photo_shoot_17400/wsgi.py
+++ backend/ag_doll_photo_shoot_17400/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ag_doll_photo_shoot_17400.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ag_doll_photo_shoot_17400.settings")
 
 application = get_wsgi_application()
