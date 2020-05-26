3c81b5e326683149d8b736bfdb83b881da33967c
Crowdbotics <team@crowdbotics.com>
2020-05-26 03:40:52 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/ag_doll_photo_shoot_17400/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ag_doll_photo_shoot_17400;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "ag_doll_photo_shoot_17400";
+    }
+}
